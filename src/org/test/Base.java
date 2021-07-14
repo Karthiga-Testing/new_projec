121a87fc37a4514f5a4de0b7e5eede459ff2d941
Karthiga-Testing <karthikathara94@gmail.com>
2021-07-14 12:26:54 +0530

    msh

---
+++ src/org/test/Base.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class Base {
+
+	private void name() {
+	System.out.println("karthu");
+
+	}
+	
+	
+	public static void main(String[] args) {
+		 System.out.println("karthiga");
+  Base b= new Base();
+  b.name();
+	}
+
+}
---
+++ src/org/test/Sample.java
@@ -0,0 +1,29 @@
+package org.test;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		String s="java programming";
+		StringBuffer s1= new StringBuffer(s);
+	  int uc=0, lc=0;
+	  
+	  for(int i=0;i<s.length();i++) {
+		  char ch = s.charAt(i);
+		  if(Character.isUpperCase(ch)) {
+			  s1.setCharAt(i, Character.toLowerCase(ch));
+			  lc++;
+		  }
+		  else if(Character.isLowerCase(ch)) {
+			  s1.setCharAt(i, Character.toUpperCase(ch));
+			  uc++;
+		  }
+		  
+	  }
+		System.out.println(s1);
+		 System.out.println("uc"+uc);
+		 System.out.println("lc"+lc);
+		
+		
+	}
+
+}
