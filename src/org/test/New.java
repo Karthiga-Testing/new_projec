5aab4d70c53aab71d30e3492281070a617ec7a28
Karthiga-Testing <karthikathara94@gmail.com>
2021-07-14 12:52:10 +0530

    jads

---
+++ src/org/test/New.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class New {
+	public static void main(String[] args) {
+		System.out.println(" String is a class");
+		System.out.println(" java has two default package");
+		System.out.println(" java.lang, java.util");
+	}
+
+}
